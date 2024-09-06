d98a6ae6c5fa835cd672099bf1eb8e37a0adc4e6
hmirae <hmira@bancolombia.com>
2024-09-06 09:38:45 -0500

    avance taller calificable

---
+++ tallerbancodigital/tallerbancodigital/src/main/java/com/bancolombia/tallerbancodigital/TallerbancodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.bancolombia.tallerbancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerbancodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerbancodigitalApplication.class, args);
+	}
+
+}
---
+++ tallerbancodigital/tallerbancodigital/src/test/java/com/bancolombia/tallerbancodigital/TallerbancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.tallerbancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerbancodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
